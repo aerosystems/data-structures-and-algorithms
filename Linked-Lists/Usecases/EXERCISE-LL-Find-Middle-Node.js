76df7a8d1bdc97b0551b92671b6af3665b1874c3
artk0st <art_k0st@icloud.com>
2024-01-06 01:54:32 +0200

    EXERCISE-LL-Find-Middle-Node

--- Linked-Lists/Usecases/EXERCISE-LL-Find-Middle-Node.js
+++ Linked-Lists/Usecases/EXERCISE-LL-Find-Middle-Node.js
@@ -54,9 +54,14 @@ class LinkedList {
     }
 
     findMiddleNode() {
-        
+        let slow = this.head;
+        let fast = this.head;
+        while (fast !== null && fast.next !== null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
-
 }
 
 
